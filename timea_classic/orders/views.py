3a33ef4f6f13ef77b8151f3ace61bdbb00b9f8df
bryansine <bryansine1738@gmail.com>
2025-01-11 19:28:53 +0300

    updates

--- timea_classic/orders/views.py
+++ timea_classic/orders/views.py
@@ -2,92 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Order, OrderItem
 from django.contrib.auth.decorators import login_required
 from cart.models import Cart, CartItem
-
 from django.http import HttpResponse
-
-
-
-
-# @login_required
-# def create_order(request):
-#     cart = request.user.cart
-#     if not cart.items.exists():
-#         return redirect('cart:view')
-
-#     if request.method == 'POST':
-#         shipping_address = request.POST.get('shipping_address')
-#         phone_number = request.POST.get('phone_number')
-
-#         # Create Order
-#         order = Order.objects.create(
-#             user=request.user,
-#             shipping_address=shipping_address,
-#             phone_number=phone_number,
-#             status='Pending'
-#         )
-
-#         # Add items from cart to order
-#         for item in cart.items.all():
-#             OrderItem.objects.create(
-#                 order=order,
-#                 product=item.product,
-#                 variant=item.variant,
-#                 quantity=item.quantity,
-#                 price=item.product.price if item.product else item.variant.price
-#             )
-
-#         # Clear the cart
-#         cart.items.all().delete()
-
-#         return redirect('orders:order_detail', order_id=order.id)
-
-#     return render(request, 'orders/create_order.html', {'cart': cart})
-
-
-# from django.db import transaction  # To handle atomic transactions
-
-# @login_required
-# def create_order(request):
-#     cart = request.user.cart
-#     if not cart.items.exists():
-#         return redirect('cart:view')
-
-#     if request.method == 'POST':
-#         shipping_address = request.POST.get('shipping_address')
-#         phone_number = request.POST.get('phone_number')
-
-#         # Use a transaction to ensure atomicity
-#         with transaction.atomic():
-#             # Create Order
-#             order = Order.objects.create(
-#                 user=request.user,
-#                 shipping_address=shipping_address,
-#                 phone_number=phone_number,
-#                 status='Pending'
-#             )
-
-#             # Track cart items to remove
-#             cart_items_to_remove = []
-
-#             # Add items from cart to order
-#             for item in cart.items.all():
-#                 OrderItem.objects.create(
-#                     order=order,
-#                     product=item.product,
-#                     variant=item.variant,
-#                     quantity=item.quantity,
-#                     price=item.product.price if item.product else item.variant.price
-#                 )
-#                 cart_items_to_remove.append(item.id)
-
-#             # Remove ordered items from cart
-#             CartItem.objects.filter(id__in=cart_items_to_remove).delete()
-
-#         return redirect('orders:order_detail', order_id=order.id)
-
-#     return render(request, 'orders/create_order.html', {'cart': cart})
-
 from django.db import transaction
+
+
 @login_required
 def create_order(request):
     cart = request.user.cart  # Ensure this is correct for your app
@@ -98,7 +16,6 @@ def create_order(request):
         shipping_address = request.POST.get('shipping_address')
         phone_number = request.POST.get('phone_number')
 
-        # Get the selected cart items (those that were checked)
         selected_cart_items = request.POST.getlist('cart_items')  # List of selected cart item IDs
 
         # Ensure selected_cart_items is a list of integers
