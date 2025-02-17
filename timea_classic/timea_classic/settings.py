36c4e3ca8dd28542dfb1b6bdba891ba817eeadc3
bryansine <bryansine1738@gmail.com>
2025-02-17 23:58:10 +0300

    updates

--- timea_classic/timea_classic/settings.py
+++ timea_classic/timea_classic/settings.py
@@ -112,17 +112,17 @@ CHANNEL_LAYERS = {
 
 
 
-# Database configuration
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),  
-    }
-}
+ # Database configuration
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),  
+#     }
+# }
 
 DATABASES = {
     'default': {
