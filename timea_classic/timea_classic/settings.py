3ab230cec1a551b7d14ce6c51839da99e9b9509e
bryan sine <bryansine1738@gmail.com>
2025-03-21 17:47:51 +0300

    caching using redis

--- timea_classic/timea_classic/settings.py
+++ timea_classic/timea_classic/settings.py
@@ -125,13 +125,13 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3', 
-    }
-}
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3', 
+#     }
+# }
 
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
