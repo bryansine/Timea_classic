b38cc6c3c219a788d17f60ee1412cbf2533918fc
bryan sine <bryansine1738@gmail.com>
2025-03-25 17:29:02 +0300

    user profile

--- timea_classic/products/admin.py
+++ timea_classic/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, ProductVariant
+from .models import Product, Category, ProductVariant, ProductReview  # Import ProductReview
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -17,3 +17,9 @@ class ProductVariantAdmin(admin.ModelAdmin):
     list_display = ('product', 'color_name', 'stock_quantity', 'is_in_stock')
     list_filter = ('color_name',)
     search_fields = ('product__name', 'color_name')
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'rating', 'created_at')
+    list_filter = ('product', 'user', 'rating')
+    search_fields = ('comment', 'user__username', 'product__name')
