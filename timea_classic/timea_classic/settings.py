1c60ce03a8daf04eb3c6189acebee753885babfd
bryan sine <bryansine1738@gmail.com>
2025-03-31 18:47:43 +0300

    aws s3 setup for serving media files

--- timea_classic/timea_classic/settings.py
+++ timea_classic/timea_classic/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'chat',
     'daraja',
     'django_daraja',
+    'storages',
 ]
 
 
@@ -183,10 +184,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
-# WhiteNoise storage backend for serving static files efficiently
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-
 # logging directories
 LOGIN_URL = '/login/'  
 LOGOUT_URL = '/logout/' 
@@ -256,3 +253,18 @@ MPESA_VALIDATION_URL = config("MPESA_VALIDATION_URL")
 
 # STK Push Callback
 MPESA_CALLBACK_URL = config("MPESA_CALLBACK_URL")
+
+
+# AWS S3 settings
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')  # e.g., 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
+STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
+# WhiteNoise storage backend for serving static files efficiently
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
