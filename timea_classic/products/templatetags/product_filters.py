5270a9b2d0c8196e6b53963a5631072bdf46879e
bryan sine <bryansine1738@gmail.com>
2025-04-09 18:58:00 +0300

    updates

--- timea_classic/products/templatetags/product_filters.py
+++ timea_classic/products/templatetags/product_filters.py
@@ -16,3 +16,27 @@ def get_item(queryset, rating):
 @register.filter
 def multiply(value, arg):
     return value * arg
+
+
+# from django import template
+
+# register = template.Library()
+
+# @register.filter
+# def less_than(value, arg):
+#     return value < arg
+
+# @register.filter
+# def get_item(queryset, rating):
+#     print(f"get_item called with queryset: {queryset}, rating: {rating}")
+#     for item in queryset:
+#         print(f"  Checking item: {item}")
+#         if item['rating'] == int(rating):
+#             print(f"  Found count: {item['count']}")  # Debugging print
+#             return item['count']
+#     print(f"  Rating {rating} not found.")
+#     return 0
+
+# @register.filter
+# def multiply(value, arg):
+#     return value * arg
