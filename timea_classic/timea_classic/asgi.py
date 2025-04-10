486d97819bea91fcdae8c82a509a1de6aa2c497a
bryan sine <bryansine1738@gmail.com>
2025-04-10 18:10:04 +0300

    Update .gitignore to ignore media files

--- timea_classic/timea_classic/asgi.py
+++ timea_classic/timea_classic/asgi.py
@@ -1,23 +1,20 @@
 import os
-from decouple import config  # Import config from decouple
+from decouple import config
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import chat.routing  # Import your chat app's routing
+import chat.routing
 
-# Use python-decouple to load the settings from the .env file
+
 settings_module = config('DJANGO_SETTINGS_MODULE', default='timea_classic.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)  # Set the environment variable for Django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
-# Application definition
 application = ProtocolTypeRouter({
-    # HTTP request handler
     'http': get_asgi_application(),
 
-    # WebSocket request handler (real-time communication for the chat app)
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns  # Your chat app's WebSocket URL patterns
+            chat.routing.websocket_urlpatterns
         )
     ),
 })
