ef5d8a3d725b6176fec84b14e93da498382d138c
bryan sine <bryansine1738@gmail.com>
2025-04-23 20:10:47 +0300

    updates

--- timea_classic/core/urls.py
+++ timea_classic/core/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('subscribe/', subscribe_newsletter, name='subscribe_newsletter'),
     path('password/change/', views.change_password, name='change_password'),
 
-    #password reset urls
+    #password reset
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='core/password_reset.html'), name='password_reset'),
     path('password-change/', auth_views.PasswordChangeView.as_view(template_name='core/password_change.html'), name='password_change'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'), name='password_reset_done'),
