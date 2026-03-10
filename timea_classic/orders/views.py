6495d7b086ed30d3c7ecf0cade5b0e53f8cbe330
bryansine <bryansine1738@gmail.com>
2026-03-10 17:45:48 +0300

    debug

--- timea_classic/orders/views.py
+++ timea_classic/orders/views.py
@@ -15,7 +15,6 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-#from django.shortcuts import render, get_object_or_404, redirect
 from daraja.utils import get_mpesa_access_token, generate_password, get_timestamp
 
     
