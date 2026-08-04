370ab053e008e157cacbeaa8da9a76d38e0b6e07
bryansine <bryansine1738@gmail.com>
2026-08-04 17:01:32 +0300

    refinements

--- timea_classic/chat/context_processors.py
+++ timea_classic/chat/context_processors.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from django.conf import settings
 
 def chat_room(request):
     if request.user.is_authenticated:
         room_name = request.user.username
         return {'chat_room_name': room_name}
     return {}
+
